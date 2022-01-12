a030b2491a1578ea860bf08d6142c34e5a5ea3fe
Revelation <revnyirongo@live.com>
2022-01-12 16:41:47 +0200

    Update number of registered households query
    Include time interval of all registered and active households

--- number of households per facility.sql
+++ number of households per facility.sql
@@ -19,3 +19,8 @@ Chifunga	886
 Dambe	        2875
 Neno DHO	4522
 */
+
+/*Time Bound; Using Contact View Metada to Calculate all households exclusive of muted*/
+SELECT ch.parent_parent_parent_name, count(distinct(x.contact_uuid)) FROM public.temp_contactview_metadata x join chw_hierarchy ch
+on x.parent_uuid = ch.parent_uuid WHERE x.contact_type IN ('household') and x.reported <= '2021-12-31' and (x.muted isnull or x.muted >= '2021-12-31')
+group by ch.parent_parent_parent_name
